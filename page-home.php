dd4227ed7275cc12152e1b278b42848040c73606
Matt Hummel <marketing@users.noreply.github.com>
2020-03-21 12:38:46 -0400

    Update
    Remove width 100% from medium-columns

--- page-home.php
+++ page-home.php
@@ -15,7 +15,212 @@
 get_header(); ?>
 
 
-<?php get_template_part( 'template-parts/content', 'promotions' );  ?>
+<?php //get_template_part( 'template-parts/content', 'promotions' );  ?>
+
+<div class="row expanded">
+
+      <div class="card green">
+
+        <div class="small-12 medium-6">
+          
+            <h1 clsas="text-uppercase">This summer learning continues online</h1>
+
+            <p>Amazing things happen when you connect with our expert fqaculty in our virtual classrooms.  Fully accredited and top-ranked, you can trust an online Germanna education will take you places</p>
+
+            <a href="/online-classes/">Get Started</a>
+
+      </div>
+
+    </div>
+
+</div>
+
+<div class="row expanded"> 
+  <div class="columns small-12 medium-6">
+
+      <div class="card yellow">
+        
+          <h2>Online summer classes start May 26</h2>
+
+          <a href="" class="button">classes</a>
+
+      </div>
+
+  </div>
+  <div class="columns small-12 medium-6">
+
+    <div class="card black">
+    
+      <h2>Fall class registration starts April 1</h2>
+      
+      <a href="" class="button">remind me</a>
+
+    </div>
+
+  </div>
+</div>
+
+
+<div class="row expanded image-row" style="background-image:('');">
+
+  <div class="card red">
+  
+      <div class="columns small-12 medium-6">
+        
+       <h2>college transfer</h2>
+
+       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+      </div>
+
+      <div class="columns small-12 medium-6">
+        
+        <ul>
+          <li>45+ transfer partners</li>
+          <li>#1 in umw transfers</li>
+          <li>1/3 cost of most va 4 year schools</li>
+        </ul>
+
+      </div>
+
+  </div>
+
+</div>
+
+<div class="row expanded">
+
+    <div class="columns small-12 medium-6">
+
+      <a href="">
+
+        <div class="card black">
+          
+          <h2>recognized</h2>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+        </div>
+
+      </a>
+
+  </div>
+
+  <div class="columns small-12 medium-6">
+
+      <a href="">
+        <div class="card green">
+          
+          <h2>our people are the difference</h2>
+
+              <ul class="list-unstyled">
+                <li>lorem ipsum</li>
+                <li>lorem ipsum</li>
+                <li>lorem ipsum</li>
+                <li>lorem ipsum</li>
+                <li>lorem ipsum</li>
+              </ul>
+
+        </div>
+      </a>
+    
+  </div>
+  
+</div>
+
+
+<div class="row expanded image-row" style="background-image:('');">
+
+  <div class="card green">
+
+    <div class="row expanded no-gutters">
+
+    <div class="columns small-12 medium-6">
+
+      <ul>
+        <li><a href="">Arts & Humanities</a></li>
+        <li><a href="">Business</a></li>
+        <li><a href="">Education & Social Science</a></li>
+        <li><a href="">Healthcare</a></li>
+        <li><a href="">It & Cybersecurity</a></li>
+        <li><a href="">Public Service</a></li>
+        <li><a href="">Science & Engineering</a></li>
+        <li><a href="">Technical Studies</a></li>
+      </ul>
+      
+    </div>
+    <div class="columns small-12 medium-6">
+
+      <h2>Career training</h2>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+    </div>
+  </div>
+  </div>
+
+</div>
+
+<div class="row expanded">
+
+  <div class="columns small-12 medium-6">
+
+    <a href="">
+
+      <div class="card green">
+        
+         <h2>germanna cares</h2>
+
+      </div>
+
+    </a>   
+
+  </div>
+
+  <div class="columns small-12 medium-6">
+
+     <a href="">
+
+      <div class="card green">
+        
+         <h2>save big</h2>
+
+      </div>
+
+    </a> 
+    
+  </div>
+
+</div>
+
+<div class="row expanded image-row" style="background-image:('');">
+  
+  <div class="card red">
+    <div class="columns small-12 medium-6">
+
+      <ul>
+        <li><a href="">Arts & Humanities</a></li>
+        <li><a href="">Business</a></li>
+        <li><a href="">Education & Social Science</a></li>
+        <li><a href="">Healthcare</a></li>
+        <li><a href="">It & Cybersecurity</a></li>
+        <li><a href="">Public Service</a></li>
+        <li><a href="">Science & Engineering</a></li>
+        <li><a href="">Technical Studies</a></li>
+      </ul>
+      
+
+    </div>
+    <div class="columns small-12 medium-6">
+
+      <h2>Career training</h2>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+    </div>
+  </div>
+
+
+</div>
 
 	<?php // get_template_part( 'template-parts/content', 'pathways' );  ?>
 <!-- 	<?php //get_template_part( 'template-parts/content', 'getstarted' ); ?>
