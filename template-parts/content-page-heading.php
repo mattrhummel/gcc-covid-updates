b98d193850ffec2847417162d2e1762d285c604e
Matt Hummel <marketing@users.noreply.github.com>
2020-03-23 11:56:12 -0400

    Update

--- template-parts/content-page-heading.php
+++ template-parts/content-page-heading.php
@@ -1,15 +1,13 @@
   <header>
-    <div class="hero-section-text">
+    <div class="row">
       <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
     </div>
-    <div class="row expanded crumbs-container gutter-small">
-      <div class="small-12 medium-8 large-9 columns">
+    <div class="row">
         <nav aria-label="<?php _e('You are here:', 'gcc-wp-2018');?>">
           <?php the_breadcrumb() ?>
         </nav>
       </div>
-      <div class="small-12 medium-4 large-3 columns show-for-landscape">
+ <!--      <div class="small-12 medium-4 large-3 columns show-for-landscape">
         <?php gcc_wp_2018_page_icons() ?>
-      </div>
-    </div>
+      </div> -->
 </header>
