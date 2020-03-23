c0edfd621cd0f5bc07c8e01f464219aad965b182
Matt Hummel <marketing@users.noreply.github.com>
2020-03-23 13:00:47 -0400

    Update

--- kitchen-sink.php
+++ kitchen-sink.php
@@ -258,12 +258,12 @@ get_header(); ?>
       <hr>
       <h2>Callouts</h2>
      <div class="callout">
-  <h2>This is a secondary callout</h2>
+  <h2>This is a standard callout</h2>
   <p>It has an easy to override visual style, and is appropriately subdued.</p>
   <a href="#">It's dangerous to go alone, take this.</a>
 </div>
      <div class="callout small primary">
-  <h2>This is a secondary callout</h2>
+  <h2>This is a small primary callout</h2>
   <p>It has an easy to override visual style, and is appropriately subdued.</p>
   <a href="#">It's dangerous to go alone, take this.</a>
 </div>
@@ -273,17 +273,17 @@ get_header(); ?>
   <a href="#">It's dangerous to go alone, take this.</a>
 </div>
      <div class="callout small alert">
-  <h2>This is a secondary callout</h2>
+  <h2>This is a alert callout</h2>
   <p>It has an easy to override visual style, and is appropriately subdued.</p>
   <a href="#">It's dangerous to go alone, take this.</a>
 </div>
      <div class="callout small success">
-  <h2>This is a secondary callout</h2>
+  <h2>This is a success callout</h2>
   <p>It has an easy to override visual style, and is appropriately subdued.</p>
   <a href="#">It's dangerous to go alone, take this.</a>
 </div>
-     <div class="callout small warning">
-  <h2>This is a secondary callout</h2>
+<div class="callout small warning">
+  <h2>This is a warning callout</h2>
   <p>It has an easy to override visual style, and is appropriately subdued.</p>
   <a href="#">It's dangerous to go alone, take this.</a>
 </div>
