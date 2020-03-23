9fc2cdaa5b5865ba3831ae5fd345c5cc308144af
Matt Hummel <marketing@users.noreply.github.com>
2020-03-23 12:33:56 -0400

    Update
    Breadcrumbs

--- template-parts/content-page-heading.php
+++ template-parts/content-page-heading.php
@@ -1,12 +1,10 @@
   <header>
-    <div class="row">
       <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-    </div>
-    <div class="row">
-        <nav aria-label="<?php _e('You are here:', 'gcc-wp-2018');?>">
+
+  
           <?php the_breadcrumb() ?>
-        </nav>
-      </div>
+   
+
  <!--      <div class="small-12 medium-4 large-3 columns show-for-landscape">
         <?php gcc_wp_2018_page_icons() ?>
       </div> -->
