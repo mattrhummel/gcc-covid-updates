cfb1bb4a23a8ef2feb40c09dac15749095fb8ad9
Matt Hummel <marketing@users.noreply.github.com>
2020-03-23 17:20:28 -0400

    Updates

--- 404.php
+++ 404.php
@@ -6,28 +6,17 @@
 */
 get_header(); ?>
 <div class="row expanded hero-section-404">
+	
 	<?php
 	$error_page_heading = get_field('error_page_heading', 'option');
 	$error_page_error_message_1 = get_field('error_page_error_message_1', 'option');
 	$error_page_error_message_2 = get_field('error_page_error_message_2', 'option');
 	$error_page_button_text = get_field('error_page_button_text', 'option');
 	$error_page_button_url = get_field('error_page_button_url', 'option');
-	$error_page_background_image = get_field('error_page_background_image', 'option');
-					// vars
-					$url = $error_page_background_image['url'];
-					$title = $error_page_background_image['title'];
-					$alt = $error_page_background_image['alt'];
-					$caption = $error_page_background_image['caption'];
-					// thumbnail
-					$size = 'large';
-					$thumb = $error_page_background_image['sizes'][ $size ];
-					$width = $error_page_background_image['sizes'][ $size . '-width' ];
-					$height = $error_page_background_image['sizes'][ $size . '-height' ];
-	if( !empty($error_page_background_image) ):   ?>
-	<img src="<?php echo $error_page_background_image['url']; ?>" alt="<?php echo $error_page_background_image['alt']; ?>" width="<?php echo $width; ?>" height="<?php echo $height; ?>" class="responsive" />
-	<?php endif; ?>
-	<div class="row expanded hero-section-404-text">
-		<div class="small-8 small-offset-2 columns">
+	?>				
+		
+	<div class="row hero-section-404-text">
+	
 			<h1 class="page-title"><?php echo $error_page_heading; ?></h1>
 			<?php
 			if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
@@ -54,18 +43,23 @@ get_header(); ?>
 			<p><?php echo $error_page_error_message_2;  ?></p>
 			<p><a href="<?php echo $error_page_button_url; ?>" class="button primary"><?php echo $error_page_button_text; ?></a>
 		</p>
-		<form role="search" method="get" id="searchform-404" style="position: relative; margin-top: 2rem;" action="<?php echo get_home_url(); ?>/search_gcse/">
-			<input type="hidden" name="cx" value="015787986713984774933:no8dqwkyepy" title="hidden" >
-			<input type="hidden" name="ie" value="utf8" title="hidden" />
-			<input type="hidden" name="oe" value="utf8" title="hidden" />
-			<input type=hidden name=domains value="<?php echo get_home_url();     //get the domain base for the search submit?>" title="home" />
-			<input type=hidden name=sitesearch value="<?php echo get_home_url(); //get the url base for the search submit?>" title="home"  />
-			<input type="text" name="q" id="search-404" title="search input" class="sb-search-input" placeholder="<?php _e('Search germanna.edu', 'gcc-wp-2018');?>" style="height: 49px;">
-			<input id="searchsubmit-404" class="sb-search-submit" type="submit" value="" aria-label="<?php _e('Submit', 'gcc-wp-2018')?>">
-			<button type="submit" id="searchsubmit-button-404" class="sb-icon-search" aria-label="Submit">
-			</button>
-		</form>
-	</div>
+
+    <form role="search" method="get" id="searchform-desktop" action="<?php echo get_home_url(); ?>/search_gcse/">
+        <div class="input-group">
+          <input type="hidden" name="cx" value="015787986713984774933:no8dqwkyepy" title="hidden">
+          <input type="hidden" name="ie" value="utf8" title="hidden" />
+          <input type="hidden" name="oe" value="utf8" title="hidden" />
+          <input type=hidden name=domains value="<?php echo get_home_url();     //get the domain base for the search submit?>" title="home" />
+          <input type=hidden name=sitesearch value="<?php echo get_home_url(); //get the url base for the search submit?>" title="home"  />
+          
+          <input type="text" name="q" id="search-field" class="input-group-field">
+          
+          <div class="input-group-button">
+            <button id="searchsubmit-mobile" type="submit" class="button" value="" aria-label="<?php _e('fa fa-eyeglass', 'gcc-wp-2018')?>"><span class="fa fa-search"></span></button>
+          </div>
+        </div>
+    </form>
+	
 </div>
 </div>
 <?php
--- template-parts/content-page-heading.php
+++ template-parts/content-page-heading.php
@@ -1,11 +1,4 @@
-  <header>
+<header>
       <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-  
-          <?php the_breadcrumb() ?>
-   
-
- <!--      <div class="small-12 medium-4 large-3 columns show-for-landscape">
-        <?php gcc_wp_2018_page_icons() ?>
-      </div> -->
+      <?php the_breadcrumb() ?> 
 </header>
