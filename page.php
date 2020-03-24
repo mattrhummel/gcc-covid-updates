13189ce4af8a61d570cd81042042d6d778083de0
Matt Hummel <marketing@users.noreply.github.com>
2020-03-24 10:15:48 -0400

    Update
    Delete unneeded tempate files

--- page.php
+++ page.php
@@ -34,6 +34,12 @@ get_header(); ?>
           get_template_part( 'template-parts/content', 'page-alert' );
           ?>
 
+          <?php if (is_page('12255')) : ; ?>
+
+            <?php putRevSlider( 'diversity-slider' ); ?>
+
+          <?php endif; ?>
+
           <?php
           the_content();
           ?>
@@ -110,7 +116,199 @@ get_header(); ?>
 
 
         <?php endif; ?>
-      
+
+        <?php if (is_page('27725')) : ; ?>
+
+        <div class="row expanded" data-equalizer data-equalize-on="medium" id="sa-events">
+
+          <?php // get raw date
+$date = get_field('scholarship_deadline', false, false); ?>
+<?php // make date object
+$date = new DateTime($date); ?>
+
+          <?php
+          $args =  array (
+          'post_type' => 'local_scholarships',
+          'posts_per_page'=> -1,
+          );
+          ?>
+          <?php
+          $query = new WP_Query( $args ); ?>
+          <?php if ( $query->have_posts() ) : ?>
+          <?php while ( $query->have_posts() ) : $query->the_post();?>
+
+              
+              <?php the_title('<h2>', '</h2>', 'gcc-wp-2018'); ?> 
+
+              <p><?php the_field( 'scholarship_short_description' ); ?></p>
+
+              <p><strong>Scholarship Deadline:</strong> <?php the_field( 'scholarship_deadline' ); ?></p>
+
+              <?php if( get_field('scholarship_information') ): ?>
+
+              <ul class="accordion" data-accordion data-allow-all-closed="true">
+              
+                <li class="accordion-item" data-accordion-item>
+                  <a href="#" class="accordion-title">Scholarship Details</a>
+                    <div class="accordion-content" data-tab-content>
+                      
+                      <?php the_field( 'scholarship_information' ); ?>
+
+                    </div>
+                  </li>
+              
+              </ul>
+
+              <?php endif; ?>
+
+           <?php if( get_field('scholarship_application_url') ): ?>
+             
+              <a href="<?php the_field( 'scholarship_application_url' ); ?>"><?php the_field( 'scholarship_application_button_text' ); ?></a>
+
+            <?php endif; ?>
+
+
+
+            <?php if( get_field('scholarship_information_url') ): ?>
+
+              |            
+
+              <a href="<?php the_field( 'scholarship_information_url' ); ?>"><?php the_field( 'scholarship_information_button_text' ); ?></a>
+
+            <?php endif; ?>
+
+        <hr>
+                      
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+        <p><?php esc_html_e( 'Sorry, no scholarships display', 'gcc-wp-2018'); ?></p>
+        <?php endif; ?>
+      </div>
+            
+          <?php endif; ?>     
+
+
+<?php if (is_page('27603')) : ; ?>
+
+        <div class="row expanded" data-equalizer data-equalize-on="medium" id="sa-events">
+          <?php
+          $args =  array (
+          'post_type' => 'sa_events',
+          'meta_key'      => 'event_date',
+          'orderby'     => 'meta_value',
+          'order'       => 'ASC',
+          'posts_per_page'=> -1,
+          );
+          ?>
+          <?php
+          $query = new WP_Query( $args ); ?>
+          <?php if ( $query->have_posts() ) : ?>
+          <?php while ( $query->have_posts() ) : $query->the_post();?>
+          <div class="small-12 medium-12 large-6 columns">
+            <div class="callout primary small"  data-equalizer-watch>
+              
+              <?php the_title('<h2>', '</h2>', 'gcc-wp-2018'); ?>
+
+              <p  style="margin-bottom: 0;"><?php the_field( 'event_date' ); ?></p>
+
+              <p style="margin-bottom: 0;"><?php the_field( 'event_location' ); ?></p>
+
+              <?php if( get_field('event_start_time') ): ?>
+              <p><?php the_field('event_start_time'); ?>   <?php endif; ?> 
+              <?php if( get_field('event_end_time') ): ?> <?php _e('- ', 'gcc-wp-2018'); ?><?php the_field('event_end_time'); ?>
+                 <?php endif; ?> 
+              </p>
+
+              <p style="margin-bottom: 0;"><?php the_field( 'event_description' ); ?></p>
+
+              <?php if( get_field('event_details') ): ?>
+              <a href="<?php echo the_permalink(); ?>" class="button primary"><?php _e('Learn more', 'gcc-wp-2018'); ?></a>
+              <?php endif; ?>
+            </div>
+            
+          </div>
+          
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+        <p><?php esc_html_e( 'Sorry, no events to display', 'gcc-wp-2018'); ?></p>
+        <?php endif; ?>
+      </div>
+
+<?php endif; ?>
+
+<?php if (is_page('26400')) : ; ?>
+
+<h2><?php _e('Included Pathways', 'gcc-wp-2018'); ?></h2>
+
+    <h3 class="text-center"><?php the_field('pathways_heading'); ?></h3>
+            
+        <?php
+        $args =  array (
+        'post_type' => 'pathways_home',     
+        'posts_per_page' => -1,
+        'orderby'=> 'title', 
+        'order' => 'ASC',
+        'tax_query'      => array (
+        
+        // Note: tax_query expects an array of arrays!
+        array(
+            'taxonomy' => 'pathway_categories', // my guess
+            'field'    => 'id',
+            'terms'    => 1823,
+        ),
+    ),
+
+        );
+        ?>
+        <?php $query = new WP_Query( $args ); ?>
+        <?php if ( $query->have_posts() ) : ?>
+         
+        <?php while ( $query->have_posts() ) : $query->the_post();?>
+             
+              <?php
+              $pathway_image = get_field('pathway_image');
+              if( !empty($pathway_image) ):
+                // vars
+                $url = $pathway_image['url'];
+                $alt = $pathway_image['alt'];
+                // thumbnail
+                $size = 'large';
+                $thumb = $pathway_image['sizes'][ $size ];
+                $width = $pathway_image['sizes'][ $size . '-width' ];
+                $height = $pathway_image['sizes'][ $size . '-height' ];
+              ?>
+  
+          <div class="row expanded">     
+          
+          <?php the_title('<h2>', '</h2>', 'gcc-wp-2018'); ?>
+
+          <div class="small-12 medium-6 large-6 columns">    
+
+          <?php the_excerpt(); ?> 
+
+         <p><a href="<?php the_field('pathway_url');?>"><?php _e('View Programs', 'gcc-wp-2018') ?></a></p>
+
+          </div>
+
+          <div class="small-12 medium-6 columns">     
+          
+          <img src="<?php echo $thumb; ?>" alt="<?php echo $alt;?>" width="<?php echo $width;?>" height="<?php echo $height;?>" style="width: 100%; background-size: cover; background-position: top center;">
+              <?php endif; ?>
+          </div>
+
+          </div>
+          <hr>
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>
+        <?php else : ?>
+        <p><?php esc_html_e( 'Sorry, no posts matched your criteria.', 'gcc-wp-2018' ); ?></p>
+        
+        <?php endif; ?>
+
+      <?php endif; ?>
+
       </div>
     </div>
     
