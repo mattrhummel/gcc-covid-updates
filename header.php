5b2112980b7b0a00726ee12d4966bc0d694b69ef
Matt Hummel <marketing@users.noreply.github.com>
2020-03-25 19:36:12 -0400

    Updates

--- header.php
+++ header.php
@@ -32,42 +32,6 @@
       <a href="#main" class="show-on-focus"><?php esc_html_e('skip to content', 'gcc-wp-2018') ?></a>
     </div>
 
-<?php if( get_field('banner_text', 'options') ): ?>  
-<div id="bannerAnnouncement" class="bottom-banner hide-for-print" data-closable>
-      
-      <div class="row expanded">
-        <div class="row">
-
-          <div class="small-12 medium-8 columns ">
-            
-            <p><?php the_field( 'banner_text', 'options' ); ?></p>
-            
-          </div>
-
-          <div class="small-12 medium-4 columns">
-            <div class="button-group show-for-medium">
-              
-              <a href="<?php echo the_field( 'banner_button_1_url', 'options' ); ?>" class="button banner-button-hollow apply-button"> 
-                  <?php the_field( 'banner_button_1_text', 'options' ); ?>
-              </a>
-
-              <a href="<?php echo the_field( 'banner_button_2_url', 'options' ); ?>" class="button banner-button request-button"> 
-                  <?php the_field( 'banner_button_2_text', 'options' ); ?>
-              </a>
-
-            </div>
-          </div>
-          
-          <button id="closeButton" class="close-button" aria-label="Dismiss alert" type="button" data-close>
-          <span aria-hidden="true">&times;</span>
-          </button>
-
-        </div>
-      </div>
-</div>
-
-<?php endif; ?>
-
     <?php
     get_template_part( 'template-parts/content', 'weather-alert' );
     ?>
