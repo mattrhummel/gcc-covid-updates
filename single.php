89f1c1a744d242e73a3e1992bdbbbba74abe95ca
Matt Hummel <marketing@users.noreply.github.com>
2020-03-26 16:57:15 -0400

    Update
    single post

--- single.php
+++ single.php
@@ -1,31 +1,73 @@
 <?php
 /**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package gccwp-2018
- */
-
+* The template for displaying all single posts
+*
+* @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+*
+* @package gccwp-2018
+*/
 get_header(); ?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-<?php
-   while ( have_posts() ) : the_post(); ?>
-
-      <?php //Single Post Page Heading
-      get_template_part( 'template-parts/content', 'single-heading' );
-      ?>
-
-   	  <?php //Single Post Content
-      get_template_part( 'template-parts/content', 'single' );
-      ?>
-
-<?php endwhile; // End of the loop. ?>
-
-
-</article>
-
-<?php
-get_footer();
+	<?php
+	while ( have_posts() ) : the_post(); ?>
+	<div class="row expanded content-area">
+		
+		<div class="mobile-sidebar" data-responsive-toggle="section-menu" data-hide-for="large">
+			<button class="button expanded mobile-sidebar-button" type="button" data-toggle="section-menu"><?php _e('In this Section', 'gcc-wp-2018'); ?>
+			</button>
+			
+		</div>
+		<?php get_sidebar();?>
+		<div class="columns small-12 large-9 large-pull-3">
+			
+			<header>
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+				<?php if ( 'post' === get_post_type() ) : ?>
+				<p><?php the_date();?></p>
+				<?php endif; ?>
+				
+				<nav aria-label="<?php _e('You are here:', 'gcc-wp-2018');?>" role="navigation">
+					<div title="breadcrumbs trail">
+						<ul class="breadcrumbs">
+							<?php $home_page = get_the_title( get_option('page_on_front'));
+							$post_title = get_the_title( get_option('page_for_posts'));
+							?>
+							<li role="menuitem">
+								<a href="<?php echo get_site_url(); ?>">
+									<?php echo $home_page; ?>
+								</a>
+							</li>
+							<li role="menuitem">
+								<a href="<?php  echo get_permalink( get_option( 'page_for_posts' ) ); ?>">
+									<?php echo $post_title; ?>
+								</a>
+							</li>
+							<li role="menuitem">
+								<?php the_title(); ?>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</header>
+			<div class="entry-content" id="main">
+				<?php the_content(); ?>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row expanded entry-footer">
+		<footer>
+			<?php //last modified page test
+			$u_time = get_the_time('U');
+			$u_modified_time = get_the_modified_time('U');
+			if ($u_modified_time >= $u_time + 86400) {
+			echo "<p>Last modified on ";
+				the_modified_time('F j, Y');
+			"</p> "; }  ?>
+			<?php gcc_wp_2018_entry_footer(); ?>
+			<footer>
+			</div>
+			<?php endwhile; // End of the loop. ?>
+		</article>
+		<?php
+		get_footer();
--- template-parts/content-single-heading.php
+++ template-parts/content-single-heading.php
@@ -7,20 +7,12 @@
 * @package gccwp-2018
 */
 ?>
-<div class="row expanded">
-  <header class="hero-section">
-    <?php //if the child page doesn't have a featured images
-    //gcc_featured_image_on_child(); ?>
-    <div class="hero-section-text">
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-    </div>
-    <div class="post-meta">
+  <header>
+    <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php if ( 'post' === get_post_type() ) : ?>
     <p><?php the_date();?></p>
   <?php endif; ?>
-</div>
-<div class="row expanded crumbs-container gutter-small">
-     <div class="small-12 medium-8 large-9 columns">    
+  
       <nav aria-label="<?php _e('You are here:', 'gcc-wp-2018');?>" role="navigation">
       <div title="breadcrumbs trail">
         <ul class="breadcrumbs">
@@ -43,10 +35,4 @@
         </ul>
       </div>
     </nav>
-  </div>
-  <div class="small-12 medium-4 large-3 columns show-for-landscape">
-    <?php gcc_wp_2018_page_icons() ?>
-  </div>
-</div>
 </header>
-</div>
