1d082e90edfed3bcf2e503720d1138aa433c9930
Matt Hummel <marketing@users.noreply.github.com>
2020-03-27 14:22:10 -0400

    Update

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -2,16 +2,12 @@
 function gcc_wp_2018_scripts() {
 wp_enqueue_style( 'gcc-wp-2018-theme-styles' , get_template_directory_uri() . '/dist/css/style.min.css', array(), '2020323', 'all' );
 
-// Deregister the jquery version bundled with WordPress.
 wp_deregister_script( 'jquery' );
 
-// CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header.
 wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js', array(), '3.4.1', true );
 
-// Deregister the jquery-migrate version bundled with WordPress.
 wp_deregister_script( 'jquery-migrate' );
 
-// CDN hosted jQuery migrate for compatibility with jQuery 3.x
 wp_register_script( 'jquery-migrate', '//code.jquery.com/jquery-migrate-3.0.1.min.js', array('jquery'), '3.0.1', true );
 
 wp_enqueue_script( 'gcc-wp-2018-foundation', '//cdnjs.cloudflare.com/ajax/libs/foundation/6.6.1/js/foundation.min.js', array(), '20191025', true );
@@ -21,12 +17,5 @@ wp_enqueue_script( 'gcc-wp-2018-scripts', get_template_directory_uri() . '/dist/
 wp_enqueue_script( 'gcc-wp-2018-lazy-load', get_template_directory_uri() . '/dist/scripts/jquery.lazy.min.js', array(), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'gcc_wp_2018_scripts', 10, 2);
-
-//remove type tag from css and javascript
-//add_filter('style_loader_tag', 'myplugin_remove_type_attr', 10, 2);
-//add_filter('script_loader_tag', 'myplugin_remove_type_attr', 10, 2);
-// function myplugin_remove_type_attr($tag, $handle) {
-//   return preg_replace( "/type=['\"]text\/(javascript|css)['\"]/", '', $tag );
-// }
 
 ?>
