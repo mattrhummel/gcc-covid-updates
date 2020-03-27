88e19e93658c964ea8e46c7ffc0267c2eb726259
Matt Hummel <marketing@users.noreply.github.com>
2020-03-26 21:13:22 -0400

    Update
    expanded row

--- page-full.php
+++ page-full.php
@@ -11,7 +11,7 @@ while ( have_posts() ) : the_post(); ?>
   <div class="content-area">
     
     <div class="entry-content" id="main">
-      <div class="row">
+      <div class="row expanded">
         <div class="columns small-12">
           
           <?php //Page Heading
