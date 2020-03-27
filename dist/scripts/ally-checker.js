b97f033e4d7f0e0aefa599eebdd225e328afd727
Matt Hummel <marketing@users.noreply.github.com>
2020-03-27 15:34:01 -0400

    Update
    cleanup

--- dist/scripts/ally-checker.js
+++
@@ -1,3 +0,0 @@
-import a11yChecker from "a11y-checker";
-
-a11yChecker();
--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -10,11 +10,12 @@ wp_deregister_script( 'jquery-migrate' );
 
 wp_register_script( 'jquery-migrate', '//code.jquery.com/jquery-migrate-3.0.1.min.js', array('jquery'), '3.0.1', true );
 
+wp_enqueue_script( 'gcc-wp-2018-lazy-load', get_template_directory_uri() . '/dist/scripts/jquery.lazy.min.js', array(), '', true );
+
 wp_enqueue_script( 'gcc-wp-2018-foundation', '//cdnjs.cloudflare.com/ajax/libs/foundation/6.6.1/js/foundation.min.js', array(), '20191025', true );
 
 wp_enqueue_script( 'gcc-wp-2018-scripts', get_template_directory_uri() . '/dist/scripts/main.min.js', array(), '20191118', true );
 
-wp_enqueue_script( 'gcc-wp-2018-lazy-load', get_template_directory_uri() . '/dist/scripts/jquery.lazy.min.js', array(), '', true );
 }
 add_action( 'wp_enqueue_scripts', 'gcc_wp_2018_scripts', 10, 2);
 
