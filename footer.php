327c6710b00ebfb18d1caab804d24796829e5623
Matt Hummel <marketing@users.noreply.github.com>
2020-04-08 15:14:33 -0400

    Update
    newsletter cleanup

--- footer.php
+++ footer.php
@@ -80,33 +80,36 @@
   </div>
 </footer>
 
-<div class="row expanded" style="background-color: #f9f9f9;">
+<div class="row expanded collapse" id="wpforms-43666" data-equalizer>
 
-<div id="wpforms-43666">
+     <div class="columns medium-6 show-for-medium">
 
-     <div class="columns medium-6 show-for-medium" style="background-image: url('https://germannacc.staging.wpengine.com/wp-content/uploads/home-sign-up.jpg'); height: 300px; background-position: top center; background-size: cover;">
+      <div class="callout primary large signup-image" data-equalizer-watch>
+        
+      </div>
 
     </div>
 
     <div class="columns medium-6">
 
+      <div class="callout primary large" data-equalizer-watch>
+ 
         <form id="wpforms-form-43666" class="wpforms-validate wpforms-form wpforms-ajax-form" data-formid="43666" method="post" enctype="multipart/form-data" action="/form-test/">
 
-          <noscript class="wpforms-error-noscript">Please enable JavaScript in your browser to complete this form.</noscript>
+          <noscript class="wpforms-error-noscript"><?php _e('Please enable JavaScript in your browser to complete this form.'); ?></noscript>
 
-      <div class="callout primary large">
         
-           <div class="row expanded">
+           <div class="row expanded collapse">
            
-            <p class="h2">Sign-up for updates</p>
+            <p class="h2"><?php _e('Sign-up for updates', 'gcc-wp-2018') ?></p>
          
             </div>
 
           <div class="input-group">
               
-              <label for="wpforms-43666-field_1" style="display: none;">Email <span class="wpforms-required-label">*</span></label>
+              <label for="wpforms-43666-field_1" style="display: none;"><?php _e('Email', 'gcc-wp-2018') ?><span class="wpforms-required-label">*</span></label>
 
-              <input type="email" id="wpforms-43666-field_1" class="input-group-field" name="wpforms[fields][1]" placeholder="Email Address..." required>
+              <input type="email" id="wpforms-43666-field_1" class="input-group-field" name="wpforms[fields][1]" placeholder="<?php _e('Email Address...'); ?>" required>
 
             <input type="hidden" name="wpforms[id]" value="43666">
             <input type="hidden" name="wpforms[author]" value="2">
@@ -115,15 +118,13 @@
 
             <div class="input-group-button">
 
-              <button type="submit" name="wpforms[submit]" class="button" value="wpforms-submit" aria-live="assertive" data-alt-text="Sending..." data-submit-text="Submit">Submit</button>
+              <button type="submit" name="wpforms[submit]" class="button" value="wpforms-submit" aria-live="assertive" data-alt-text="Sending..." data-submit-text="Submit"><?php _e('Submit'); ?></button>
 
               <img src="https://germannacc.staging.wpengine.com/wp-content/plugins/wpforms/assets/images/submit-spin.svg" class="wpforms-submit-spinner" style="display: none;" width="26" height="26" alt="">
 
           </div>
 
         </div>
-
-      </div>
 
       </form>
 
