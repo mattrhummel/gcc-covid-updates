b95021e780654dfa1ea076bc09973e8d5b52b5fc
Matt Hummel <marketing@users.noreply.github.com>
2020-04-09 15:46:29 -0400

    Update

--- inc/cleanup.php
+++ inc/cleanup.php
@@ -27,14 +27,6 @@ endif;
 //remove plugin scripts
 if ( ! function_exists( 'gcc_wp_2018_deregister_style' ) ) :
 function gcc_wp_2018_deregister_style() {
-	wp_deregister_style( 'fb_data_style' );
-	wp_deregister_style( 'jquery-ui-css' );
-	wp_deregister_style( 'jquery-lazyloadxt-fadein' );
-	wp_deregister_style( 'wp-block-library' );
-	wp_deregister_style( 'wp-mediaelement' );
-	wp_deregister_style( 'mediaelement' );
-	wp_deregister_style( 'imgareaselect' );
-	wp_deregister_style( 'wpcdt-public-css' );
 }
 add_action( 'wp_enqueue_scripts','gcc_wp_2018_deregister_style' );
 endif;
