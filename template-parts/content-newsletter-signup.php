94e68dfc27aa3c1d41a71c4dad9340aeef9dc330
Matt Hummel <marketing@users.noreply.github.com>
2020-04-10 13:06:49 -0400

    Update

--- template-parts/content-newsletter-signup.php
+++ template-parts/content-newsletter-signup.php
@@ -8,7 +8,10 @@
     <div class="callout primary large" data-equalizer-watch>
 
       <p class="h2"><?php _e('Sign-up for updates') ?></p>
-      <?php echo do_shortcode( '[wpforms id="43666"]'); ?>
+   <!-- Begin Constant Contact Active Forms -->
+<script> var _ctct_m = "baad606721cba1bf6f3bfe1c1432a143"; </script>
+<script id="signupScript" src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js" async defer></script>
+<!-- End Constant Contact Active Forms -->
     </div>
   </div>
 </div>
