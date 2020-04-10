3d53df4e7392d148c67336410335605b87e48eaa
Matt Hummel <marketing@users.noreply.github.com>
2020-04-10 13:17:21 -0400

    Update

--- template-parts/content-newsletter-signup.php
+++ template-parts/content-newsletter-signup.php
@@ -7,9 +7,9 @@
   <div class="columns medium-6">
     <div class="callout primary large" data-equalizer-watch>
 
- <!-- Begin Constant Contact Inline Form Code -->
+<!-- Begin Constant Contact Inline Form Code -->
 <div class="ctct-inline-form" data-form-id="4208cdf9-aba0-465e-9f6d-1927eb0e1c8c"></div>
 <!-- End Constant Contact Inline Form Code -->
-    </div>
+
   </div>
 </div>
