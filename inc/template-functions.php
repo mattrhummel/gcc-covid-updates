9b99103348357d1b2474eaaaaa10b9e019ee234a
Matt Hummel <marketing@users.noreply.github.com>
2020-04-13 15:23:12 -0400

    Update
    remove archives from title

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -53,3 +53,17 @@ add_filter( 'protected_title_format', 'remove_protected_text' );
 function remove_protected_text() {
 return __('%s');
 }
+ add_filter( 'get_the_archive_title', function ($title) {    
+        if ( is_category() ) {    
+                $title = single_cat_title( '', false );    
+            } elseif ( is_tag() ) {    
+                $title = single_tag_title( '', false );    
+            } elseif ( is_author() ) {    
+                $title = '<span class="vcard">' . get_the_author() . '</span>' ;    
+            } elseif ( is_tax() ) { //for custom post types
+                $title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+            } elseif (is_post_type_archive()) {
+                $title = post_type_archive_title( '', false );
+            }
+        return $title;    
+    });
