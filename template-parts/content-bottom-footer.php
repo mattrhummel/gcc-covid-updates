ba836f5aa9ac51478bfe44227fed10c451c7b1b9
Matt Hummel <marketing@users.noreply.github.com>
2020-04-13 11:46:57 -0400

    Update
    tips

--- template-parts/content-bottom-footer.php
+++ template-parts/content-bottom-footer.php
@@ -5,11 +5,14 @@
       <div class="small-12 medium-only-12 large-7 columns">
         <?php
         gcc_wp_2018_footer_links_menu(); ?>
+        
         <div class="tips">
           
-          <a href="<?php echo get_field( 'tips_url', 'options' ); ?>" class="tips-logo" aria-hidden="true">
-            
-            <span><?php echo get_field( 'tips_text', 'options' ); ?><span class="hide-text"><?php _e('report an incident', 'gcc-wp-2018')?></span></span></a>
+            <a href="<?php echo get_field( 'tips_url', 'options' ); ?>" class="tips-logo" aria-hidden="true">
+              
+              <span class="hide-text"><?php _e('report an incident', 'gcc-wp-2018')?></span>
+
+            </a>
             
           </div>
         </div>
