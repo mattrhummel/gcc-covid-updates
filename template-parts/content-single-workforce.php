a2e8d685d63fee9848f2585d9bc68156f7d85c61
Matt Hummel <marketing@users.noreply.github.com>
2020-04-13 11:15:15 -0400

    Update
    cleanup

--- template-parts/content-single-workforce.php
+++ template-parts/content-single-workforce.php
@@ -12,42 +12,24 @@
 <div class="row gutter-small expanded content-area">
 
 	<div class="small-12 medium-8 large-9 gutter-small entry-content">
-
-
+ d
 	<div class="entry-content">
 
 	<?php
- the_content( sprintf(
- wp_kses(
-	 /* translators: %s: Name of current post. Only visible to screen readers */
-	 __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'gcc-wp-2018' ),
-	 array(
-		 'span' => array(
-			 'class' => array(),
-		 ),
-	 )
- ),
- get_the_title()
-)
+	 the_content( sprintf(
+	 wp_kses(
+		 /* translators: %s: Name of current post. Only visible to screen readers */
+		 __( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'gcc-wp-2018' ),
+		 array(
+			 'span' => array(
+				 'class' => array(),
+			 ),
+		 )
+	 ),
+	 get_the_title()
+	)
 
-); ?>
-
-
-<?php getPrevNext();
-
-get_the_category_list();
-
-?>
-
-
-<?php
-
- // If comments are open or we have at least one comment, load up the comment template.
- if ( comments_open() || get_comments_number() ) :
-	 comments_template();
- endif;
-
-?>
+	); ?>
 
 </div>
 </div>
@@ -55,13 +37,16 @@ get_the_category_list();
 <?php //Template Sidebar
 get_sidebar(); ?>
 
-
 </div><!--.pagecontent-->
-
-<?php if ( get_edit_post_link() ) : ?>
-
-	<footer class="entry-footer">
-		<?php gcc_wp_2018_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-
-<?php endif; ?>
+    <div class="row expanded entry-footer">
+        <footer>
+          <?php //last modified page test
+          $u_time = get_the_time('U');
+          $u_modified_time = get_the_modified_time('U');
+          if ($u_modified_time >= $u_time + 86400) {
+          echo "<p>Last modified on ";
+            the_modified_time('F j, Y');
+          "</p> "; }  ?>
+          <?php gcc_wp_2018_entry_footer(); ?>
+          <footer>
+     </div>
