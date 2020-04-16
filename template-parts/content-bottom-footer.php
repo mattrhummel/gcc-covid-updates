c3ba6ac0c358bd57efdd9bc8aced16134fa19c29
Matt Hummel <marketing@users.noreply.github.com>
2020-04-16 10:57:23 -0400

    update
    fix tips url

--- template-parts/content-bottom-footer.php
+++ template-parts/content-bottom-footer.php
@@ -8,7 +8,7 @@
         
         <div class="tips">
           
-            <a href="https://va.moatusers.com/IncidentReport/IncidentReport.asp?q=tkifz6aqXq0FeMvFRcEpvVGQOmy0m7q8Ntzmo0knGpzrnMbHIS&am1=EEHDDD&p=%5BTReZNaaN%5D&s=78SbSoDA0n3sbFYGRLnx&x=fC6PigX3y8PgzfbryTEXzG6zbHG2ga" class="tips-logo" aria-hidden="true">
+            <a href="https://va.moatusers.com/IncidentReport/Introduction?q=tkifz6aqXq0FeMvFRcEpvVGQOmy0m7q8Ntzmo0knGpzrnMbHIS&1=EEHDDD&p=%5BTReZNaaN%5D&s=78SbSoDA0n3sbFYGRLnx&x=fC6PigX3y8PgzfbryTEXzG6zbHG2ga" class="tips-logo" aria-hidden="true">
               
               <span class="hide-text"><?php _e('report an incident', 'gcc-wp-2018')?></span>
 
