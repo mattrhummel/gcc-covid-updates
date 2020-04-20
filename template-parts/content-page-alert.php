bd43f75b9f73ead7acc5a8513a80a49519405c3f
Matt Hummel <marketing@users.noreply.github.com>
2020-04-20 13:53:26 -0400

    Update
    change page alert

--- template-parts/content-page-alert.php
+++ template-parts/content-page-alert.php
@@ -4,7 +4,7 @@ global $post;
 if ( get_field('page_alert', $post->post_parent ) ):
 ?>
 <div class="row expanded">
-	<div class='callout announcement small hide-for-print' data-closable>
+	<div class='callout alert small hide-for-print' data-closable>
 		<?php the_field('page_alert', $post->post_parent );?>
 		<button class="close-button" aria-label="<?php esc_html_e('Dismiss alert', 'gcc-wp-2018'); ?>" type="button" data-close>
 		<span aria-hidden="true">&times;</span>
