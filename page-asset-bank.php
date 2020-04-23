4ff68f89bc9b972f34d6232b5ef1ea555b34ac2d
Matt Hummel <marketing@users.noreply.github.com>
2020-04-23 16:24:28 -0400

    update
    add tabs for various categories

--- page-asset-bank.php
+++ page-asset-bank.php
@@ -11,6 +11,7 @@ while ( have_posts() ) : the_post(); ?>
   <div class="content-area">
     
     <div class="entry-content" id="main">
+
       <div class="row expanded">
         <div class="columns small-12">
           
@@ -21,14 +22,17 @@ while ( have_posts() ) : the_post(); ?>
 
         </div>
       </div>
-      <div class="row expanded" style="padding-left: 10px; padding-right: 10px;">
 
-        <?php //Page Heading
-          get_template_part( 'template-parts/content', 'page-alert' );
-        ?>
+<ul class="tabs" data-tabs id="example-tabs">
+  <li class="tabs-title is-active"><a href="#zoom-backgrounds" aria-selected="true">Zoom Backgrounds</a></li>
+  <li class="tabs-title"><a data-tabs-target="logos" href="#logos">Logos</a></li>
+  <li class="tabs-title"><a data-tabs-target="templates" href="#templates">Templates</a></li>
+  <li class="tabs-title"><a data-tabs-target="panel2" href="#fonts">Fonts</a></li>
+</ul>
 
-      </div>
-
+<div class="tabs-content" data-tabs-content="example-tabs">
+  <div class="tabs-panel is-active" id="zoom-backgrounds">
+    
 <?php 
 $args = array(
 'post_status' => 'inherit',
@@ -50,7 +54,7 @@ $the_query = new WP_Query( $args ); ?>
 
 <?php if ( $the_query->have_posts() ) { ?>
 
-<div class="row expanded" data-equalizer>
+<div class="row expanded" data-equalizer style="padding-left: 10px; padding-right: 10px;">
 
   <h2><?php _e('Zoom Backgrounds', 'gcc-wp-2018'); ?></h2>
  
@@ -79,6 +83,70 @@ $the_query = new WP_Query( $args ); ?>
 wp_reset_postdata();
 
 ?>
+
+
+  </div>
+  <div class="tabs-panel" id="logos">
+    <?php 
+$args = array(
+'post_status' => 'inherit',
+'posts_per_page' => -1,
+'post_type' => 'attachment',
+);
+
+$args['tax_query'] = array(
+
+array(
+    'taxonomy' => 'asset_bank_categories',
+    'terms' => array( 'logo' ),
+    'field' => 'slug',
+),
+
+);
+
+$the_query = new WP_Query( $args ); ?>
+
+<?php if ( $the_query->have_posts() ) { ?>
+
+<div class="row expanded" data-equalizer style="padding-left: 10px; padding-right: 10px;">
+
+  <h2><?php _e('Zoom Backgrounds', 'gcc-wp-2018'); ?></h2>
+ 
+  <?php
+    while ( $the_query->have_posts() ) {
+    $the_query->the_post();
+        ?>
+
+ <div class="columns medium-4">
+    <div class="callout" data-equalizer-watch>
+
+    <?php echo wp_get_attachment_image( get_the_ID(), array('500', '500'), "", array( "class" => "thumbnail" ) );  ?>
+   
+    <?php $asset_image_url = wp_get_attachment_url( get_post_thumbnail_id() ); ?>
+    <a href="<?php echo $asset_image_url; ?>" download><?php _e('Download', 'gcc-wp-2018'); ?> <?php echo get_the_title(); ?></a>
+
+  </div>
+ </div>
+
+<?php  }
+}  ?>
+
+</div>
+
+<?php /* Restore original Post Data */
+wp_reset_postdata();
+
+?>
+  </div>
+   <div class="tabs-panel" id="templates">
+    <p>Suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
+  </div>
+   <div class="tabs-panel" id="fonts">
+    <p>Suspendisse dictum feugiat nisl ut dapibus.  Vivamus hendrerit arcu sed erat molestie vehicula. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor.  Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
+  </div>
+</div>
+
+
       
     </div>
   </div>
