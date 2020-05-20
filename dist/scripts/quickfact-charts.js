b48f178e0a78a0034acc6a9181549dbb4758f62c
Matt Hummel <marketing@users.noreply.github.com>
2020-05-20 13:54:25 -0400

    Update
    add location custom list for gcc at a glance page

--- dist/scripts/quickfact-charts.js
+++ dist/scripts/quickfact-charts.js
@@ -1,22 +1,66 @@
 //graduation rates
-var pieGenderAndAge = document.getElementById('genderAndAgePie');
-var genderAndAgePie = new Chart(pieGenderAndAge, {
+var pieGender = document.getElementById('genderPie');
+var genderPie = new Chart(pieGender, {
     type: 'pie',
     data: {
-    labels: ['male', 'female', 'under 25', 'over 25'],
+    labels: ['male', 'female'],
          datasets: [{
-            data: [40, 60, 77, 33],
+            data: [40, 60],
             backgroundColor: [
                 'rgb(22, 86, 160)',
                 'rgb(141, 183, 60)',
-                 'rgb(15, 64, 119)',
-                  'rgb(95, 122, 40)'
+                
             ],
             borderColor: [
              'rgb(22, 86, 160)',
                 'rgb(141, 183, 60)',
-                 'rgb(15, 64, 119)',
-                  'rgb(95, 122, 40)'  
+               
+            ],
+            borderWidth: 0
+        }]
+    },
+    plugins: [ChartDataLabels],
+    options: {
+           plugins: {
+            // Change options for ALL labels of THIS CHART
+
+            datalabels: {
+                formatter: function(value, context) {
+                    return value + '%';
+                },                
+                padding: 4,
+                color: '#ffff',
+
+                font: {
+                size: '20',
+                weight: '800'
+
+                },
+
+
+
+            }
+        },
+
+    }
+});
+//graduation rates
+var pieAge = document.getElementById('agePie');
+var agePie = new Chart(pieAge, {
+    type: 'pie',
+    data: {
+    labels: ['under 25', 'over 25'],
+         datasets: [{
+            data: [77, 33],
+            backgroundColor: [
+                'rgb(22, 86, 160)',
+                'rgb(141, 183, 60)'
+              
+            ],
+            borderColor: [
+             'rgb(22, 86, 160)',
+                'rgb(141, 183, 60)',
+                
             ],
             borderWidth: 0
         }]
