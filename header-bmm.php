8d1e983583f3a3066435394f73819b7acb3f033f
Matt Hummel <marketing@users.noreply.github.com>
2020-06-04 17:46:03 -0400

    Update
    add bmm template

---
+++ header-bmm.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* The header for our theme
+*
+* @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+*
+* @package gccwp-2018
+*/
+?>
+<!DOCTYPE html>
+<html class="no-js" <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
+    <?php
+      get_template_part( 'template-parts/content', 'analytic-scripts' );
+    ?>
+  </head>
+  
+  <body id="bmm" <?php body_class(); ?>>
+
+    <div id="skip">
+      <a href="#main" class="show-on-focus"><?php esc_html_e('skip to content', 'gcc-wp-2018') ?></a>
+    </div>
+
+    <main class="clearfix">
+
+       <a href="<?php bloginfo('url'); ?>"><img src="<?php echo get_theme_mod( 'gcc_wp_2018_mobile_logo' ); ?> " alt="white germanna logo"/></a>
---
+++ page-bmm.php
@@ -0,0 +1,37 @@
+<?php
+//Names the page template for each section
+/*
+Template Name: Black Minds Matter
+*/
+get_header( 'bmm' ); ?>
+<?php
+while ( have_posts() ) : the_post(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  
+    
+      <div class="row expanded" id="main" style="margin-top: 40px; margin-bottom: 180px;">
+
+        <div class="columns small-12">
+
+          <?php the_title('<h1> #', '</h1>', 'gcc-wp-2018'); ?>
+
+          <div class="row expanded" style="margin-top: 60px;">
+
+            <div class="columns medium-8">
+         
+             <?php
+                the_content();
+              ?>
+
+          </div>
+
+          </div>
+
+        </div>
+      
+    </div>
+
+</article>
+<?php endwhile; // End of the loop. ?>
+<?php
+get_footer( 'bmm' );
