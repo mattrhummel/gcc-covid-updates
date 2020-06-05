021d92f5817af70786dd88abca3c65de7beaa755
Matt Hummel <marketing@users.noreply.github.com>
2020-06-05 14:20:38 -0400

    Update
    add footer to bmm page

--- footer-bmm.php
+++ footer-bmm.php
@@ -1,5 +1,21 @@
 
 </main>
 
+<footer>
+
+	<div class="footer-container">
+
+<a href="<?php bloginfo('url'); ?>"><img src="https://germannacc.staging.wpengine.com/wp-content/uploads/gcc-logo-white-small.svg" alt="white germanna logo" height="68" width="117" class="logo"/></a>
+
+
+<ul>
+	<li><a href="https://www.germanna.edu/about-germanna/privacy-policy/">Privacy</a></li> |
+	<li><a href="https://www.germanna.edu/statements-policies/">Statements &amp; Policies</a></li>
+</ul>
+
+</div>
+
+</footer>
+
 </body>
 </html>
