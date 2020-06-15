d2a97408ef8f00237bf8d31df2c00f6295c738cc
Matt Hummel <marketing@users.noreply.github.com>
2020-06-15 11:06:04 -0400

    update
    fix list margin bottom

--- header.php
+++ header.php
@@ -30,35 +30,6 @@
       get_template_part( 'template-parts/content', 'weather-alert' );
     ?>
 
-<?php if (is_page('home')) : ; ?>
-
-<!-- <p><a data-open="exampleModal1">Click me for a modal</a></p>
- -->
-
-<!--   <div class="bmm-banner" data-closable>
-    
-     <div class="logo">
-
-        <a href="<?php bloginfo('url'); ?>"><img src="https://germannacc.staging.wpengine.com/wp-content/uploads/gcc-logo-white-small.svg" alt="white germanna logo" height="40" width="200"/></a>
-
-      </div>
-
-  <div class="callout secondary">
-    
-   <p><img src="https://www.germanna.edu/wp-content/uploads/black-minds-matter.png" alt="Black Minds Matter" height='45px' width="549px" /></p>
-
-    <a class="button" href="/blackmindsmatter">Learn more</a>
-    <p>
-      <button class="continue" data-close aria-label="Close modal" type="button">
-        <span class="fa fa-arrow-right" aria-hidden="true"></span>
-      Continue to germanna.edu</button>
-    </p>
-
-  </div>
-</div> -->
-
-<?php endif; ?>
-
     <div class="off-canvas-wrapper no-js">
       <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
         <?php main_menu(); //main sidebar navigation ?>
