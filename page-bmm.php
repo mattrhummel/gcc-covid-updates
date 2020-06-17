f22e439831a004f24b18836666e435e39ed79bcf
Matt Hummel <marketing@users.noreply.github.com>
2020-06-17 15:51:50 -0400

    update
    fix bmm card heading

--- page-bmm.php
+++ page-bmm.php
@@ -36,13 +36,13 @@ while ( have_posts() ) : the_post(); ?>
           $query = new WP_Query( $args ); ?>
           <?php if ( $query->have_posts() ) : ?>
 
-                      <h2>Resources</h2>
+           <h2>Resources</h2>
 
           <?php while ( $query->have_posts() ) : $query->the_post();?>
           
           <div class="card" style="padding: 1rem;">
             
-            <?php the_title('<h2 style="margin-bottom: 0; padding-bottom: 0;">', '</h2>', 'gcc-wp-2018'); ?>
+            <?php the_title('<h3 style="margin-bottom: 0; padding-bottom: 0;">', '</h3>', 'gcc-wp-2018'); ?>
             <p><?php echo get_the_date(); ?></p>
             
             <?php the_content(); ?>
@@ -54,7 +54,8 @@ while ( have_posts() ) : the_post(); ?>
           <?php else : ?>
           <p><?php esc_html_e( 'Sorry, no resources to display', 'gcc-wp-2018'); ?></p>
           <?php endif; ?>
-        </div>
+        
+         </div>
 
           </div>
 
