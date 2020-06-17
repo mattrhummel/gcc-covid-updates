c80bfbf0d6027242c43d2f7a431e9895e36eab8d
Matt Hummel <marketing@users.noreply.github.com>
2020-06-17 15:45:05 -0400

    update
    bmm add post section

--- page-bmm.php
+++ page-bmm.php
@@ -19,8 +19,40 @@ while ( have_posts() ) : the_post(); ?>
               <?php
                 the_content();
               ?>
+          
+          <h2>Resources</h2>
+          
+          <?php
+          $args =  array (
+          'post_type' => 'post',
+          'category_name' => 'black minds matter',
+          'post_status' => 'publish',
+          'orderby' => 'publish_date',
+          'order' => 'DESC',
+          'posts_per_page'=> -1,
+          );
+          ?>
+
+          <?php
+          $query = new WP_Query( $args ); ?>
+          <?php if ( $query->have_posts() ) : ?>
+          <?php while ( $query->have_posts() ) : $query->the_post();?>
+          
+          <div class="card" style="padding: 1rem;">
+            
+            <?php the_title('<h2 style="margin-bottom: 0; padding-bottom: 0;">', '</h2>', 'gcc-wp-2018'); ?>
+            <p><?php echo get_the_date(); ?></p>
+            
+            <?php the_content(); ?>
 
           </div>
+          
+          <?php endwhile; ?>
+          <?php wp_reset_postdata(); ?>
+          <?php else : ?>
+          <p><?php esc_html_e( 'Sorry, no events to display', 'gcc-wp-2018'); ?></p>
+          <?php endif; ?>
+        </div>
 
           </div>
 
