5b7f004ffeea0079cc557dae1b69d95a3e05eff1
Matt Hummel <marketing@users.noreply.github.com>
2020-07-24 11:20:14 -0400

    update
    fix animation

--- footer.php
+++ footer.php
@@ -53,6 +53,8 @@ if ($u_modified_time >= $u_time + 86400) { ?>
 get_template_part( 'template-parts/content', 'mobile-nav' );
 ?>
 
+<?php
+if ( is_page('11434') || is_page('8') || is_page('24') || is_page('44') ||  is_page('40526') ||  is_page('20') ||  is_page('44712') || is_page('44700') || is_page('44715') ) {  ?>
 
 <div class="popout-banner">
 
@@ -130,6 +132,8 @@ wp_reset_query();
 
 </div><!--.close-canvas-content-->
 </div>
+
+<?php  } ?>
 
 <?php wp_footer(); ?>
 <script>
