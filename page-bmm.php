82a50782d85622773acf96855299d33f4671d1ec
Matt Hummel <marketing@users.noreply.github.com>
2020-07-27 16:06:54 -0400

    update
    remove resource post query

--- page-bmm.php
+++ page-bmm.php
@@ -20,38 +20,9 @@ while ( have_posts() ) : the_post(); ?>
 
             </div>
 
-          <div class="columns large-8">
+          <div class="columns large-8" style="color: #ffffff;">
                    
-          <?php
-          $args =  array (
-          'post_type' => 'post',
-          'category_name' => 'black minds matter',
-          'posts_per_page'=> -1,
-          );
-          ?>
-
-          <?php
-          $query = new WP_Query( $args ); ?>
-          <?php if ( $query->have_posts() ) : ?>
-
-          <?php while ( $query->have_posts() ) : $query->the_post();?>
-          
-          <div class="card" style="padding: 1rem;">
-            
-            <?php the_title('<h2 style="margin-bottom: 0; padding-bottom: 0;">', '</h2>', 'gcc-wp-2018'); ?>
-            <p><?php echo get_the_date(); ?></p>
-            
             <?php the_content(); ?>
-
-           </div>
-          
-          <?php endwhile; ?>
-          <?php wp_reset_postdata(); ?>
-          <?php else : ?>
-          <p><?php esc_html_e( 'Sorry, no resources to display', 'gcc-wp-2018'); ?></p>
-          <?php endif; ?>
-
-          <?php the_content(); ?>
         
          </div>
 
