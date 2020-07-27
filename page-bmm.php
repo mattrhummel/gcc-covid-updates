09f476f25c3e74f3f1c30b3531f8ce22d4edb5b6
Matt Hummel <marketing@users.noreply.github.com>
2020-07-27 11:52:01 -0400

    update
    change width of container

--- page-bmm.php
+++ page-bmm.php
@@ -10,7 +10,7 @@ while ( have_posts() ) : the_post(); ?>
     
       <div class="row expanded" id="main">
 
-        <div class="columns small-12">
+        <div class="columns small-10">
 
           <div class="row expanded" style="margin-top: 60px;">
 
