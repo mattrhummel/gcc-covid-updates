79d413aa7c4bf8ee92bf468305a8aaac19d5b144
Matt Hummel <marketing@users.noreply.github.com>
2020-07-29 14:08:19 -0400

    update
    fix programs

--- page-pathways-template.php
+++ page-pathways-template.php
@@ -113,9 +113,10 @@ while ( have_posts() ) : the_post(); ?>
       $idd = get_the_ID();
       $item_classes = '';
       $item_cats = get_the_terms($post->ID, 'pathway_names');
-      if($item_cats):
+      
+      ?>
 
-          ?><table style="width: 100%;" class="stack <?php echo $item_classes  ?> item" style="min-width: 100%;">
+  <table style="width: 100%;" class="stack <?php echo $item_classes  ?> item" style="min-width: 100%;">
   <tr>
     <thead>
       <th style="width: 500px;">Program of Study</th>
@@ -126,7 +127,10 @@ while ( have_posts() ) : the_post(); ?>
   </thead>
   </tr>
         
-        foreach($item_cats as $item_cat) {
+      <?php 
+      if($item_cats):
+
+      foreach($item_cats as $item_cat) {
 
         
         ?>
