9645e7de5d3359f1063b9e5ffd788aebcee9dec5
Matt Hummel <marketing@users.noreply.github.com>
2020-08-04 15:12:52 -0400

    update
    fix filters height

--- page-pathways-template.php
+++ page-pathways-template.php
@@ -50,11 +50,11 @@ while ( have_posts() ) : the_post(); ?>
             }
             }
             ?>
-           <div class="row expanded" data-equalizer>
+           <div class="row expanded">
             <ul id="filters-large" style="list-style-type: none; margin-left: 0;">
               <li style="list-style: none;">
                 <div class="columns medium-12 xlarge-6">
-                  <a href="#" data-filter="*" class="button alert expanded text-center" data-equalizer-watch>
+                  <a href="#" data-filter="*" class="button alert expanded text-center">
                   <i class="fas fa-asterisk" style="margin-bottom: 10px;"></i><br/>All Programs
                   </a>
                 </div>
@@ -63,7 +63,7 @@ while ( have_posts() ) : the_post(); ?>
               foreach ( $program_taxs as $program_tax_slug => $program_tax_name ):   //for each term: ?>
               <li style="list-style-type: none;">
                 <div class="columns medium-12 xlarge-6">
-                  <a data-filter=".<?php echo $program_tax_slug; ?>" class="button alert expanded text-center" data-equalizer-watch>
+                  <a data-filter=".<?php echo $program_tax_slug; ?>" class="button alert expanded text-center">
                   <i class="<?php echo $program_icon->term ?>" style="margin-bottom: 10px;"></i><br/><?php echo $program_tax_name;  ?>
                 </a>
               </div>
