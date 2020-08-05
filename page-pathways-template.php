1fa8b2f83d8c3e318a25a498bbf5ef57ad490640
Matt Hummel <marketing@users.noreply.github.com>
2020-08-05 08:01:22 -0400

    update
    updated pathways template

--- page-pathways-template.php
+++ page-pathways-template.php
@@ -27,7 +27,7 @@ while ( have_posts() ) : the_post(); ?>
           <div class="show-for-xlarge">
             <h2>Select a Career Pathway</h2>          
           </div>
-          <div class="columns large-5"> 
+          <div class="columns large-6"> 
             <div class="show-for-large">          
             <?php
             $args= array(
@@ -54,77 +54,190 @@ while ( have_posts() ) : the_post(); ?>
             <ul id="filters-large" style="list-style-type: none; margin-left: 0;">
               <li style="list-style: none;">
                 <div class="columns medium-12 xlarge-6">
-                  <a href="#" data-filter="*" class="button alert expanded text-center">
-                  <i class="fas fa-asterisk" style="margin-bottom: 10px;"></i><br/>All Programs
+                  <a href="#" data-filter="*" class="h3 button alert large expanded text-center">
+                  <i class="fas fa-asterisk fa-2x" style="margin-bottom: 10px;"></i><br/>All Programs
                   </a>
                 </div>
               </li>
               <li style="list-style-type: none;">
                 <div class="columns medium-12 xlarge-6">
-                  <a data-filter=".business-pathway" class="button alert expanded text-center">
-                  <i class="fas fa-briefcase" style="margin-bottom: 10px;"></i><br/>Business</a>
+                  <a data-filter=".business-pathway" class="h3 button alert large expanded text-center">
+                  <i class="fas fa-briefcase fa-2x" style="margin-bottom: 10px;"></i><br/>Business</a>
               </div>
             </li>
               <li style="list-style-type: none;">
                 <div class="columns medium-12 xlarge-6">
-                  <a data-filter=".public-service-pathway" class="button alert expanded text-center">
-                  <i class="fas fa-gavel" style="margin-bottom: 10px;"></i><br/>Public Service</a>
+                  <a data-filter=".public-service-pathway" class="h3 button alert expanded large text-center">
+                  <i class="fas fa-gavel fa-2x" style="margin-bottom: 10px;"></i><br/>Public Service</a>
               </div>
             </li>
               <li style="list-style-type: none;">
                 <div class="columns medium-12 xlarge-6">
-                  <a data-filter=".it-and-technical-studies-pathway" class="button alert expanded text-center">
-                  <i class="fas fa-laptop-code" style="margin-bottom: 10px;"></i><br/>IT &amp; Cybersecurity</a>
+                  <a data-filter=".it-and-technical-studies-pathway" class="h3 button alert large expanded text-center">
+                  <i class="fas fa-laptop-code fa-2x" style="margin-bottom: 10px;"></i><br/>IT &amp; Cybersecurity</a>
               </div>
             </li>
               <li style="list-style-type: none;">
                 <div class="columns medium-12 xlarge-6">
-                  <a data-filter=".health-science-pathway" class="button alert expanded text-center">
-                  <i class="fas fa-user-nurse" style="margin-bottom: 10px;"></i><br/>Healthcare</a>
+                  <a data-filter=".health-science-pathway" class="h3 button alert expanded large text-center">
+                  <i class="fas fa-user-nurse fa-2x" style="margin-bottom: 10px;"></i><br/>Healthcare</a>
               </div>
             </li>
               <li style="list-style-type: none;">
                 <div class="columns medium-12 xlarge-6">
-                  <a data-filter=".social-science-and-education" class="button alert expanded text-center">
-                  <i class="fas fa-school" style="margin-bottom: 10px;"></i><br/>Education &amp; Social Science</a>
+                  <a data-filter=".social-science-and-education" class="h3 button alert expanded large text-center">
+                  <i class="fas fa-school fa-2x" style="margin-bottom: 10px;"></i><br/>Education &amp; Social Science</a>
               </div>
             </li>
                <li style="list-style-type: none;">
                 <div class="columns medium-12 xlarge-6">
-                  <a data-filter=".science-and-engineering-pathway" class="button alert expanded text-center">
-                  <i class="fas fa-atom" style="margin-bottom: 10px;"></i><br/>Science &amp; Engineering</a>
+                  <a data-filter=".science-and-engineering-pathway" class="h3 button alert expanded large text-center">
+                  <i class="fas fa-atom fa-2x" style="margin-bottom: 10px;"></i><br/>Science &amp; Engineering</a>
               </div>
             </li>
             <li style="list-style-type: none;">
                 <div class="columns medium-12 xlarge-6">
-                  <a data-filter=".humanities-and-arts-pathway" class="button alert expanded text-center">
-                  <i class="fas fa-palette" style="margin-bottom: 10px;"></i><br/>Arts &amp; Humanities</a>
+                  <a data-filter=".humanities-and-arts-pathway" class="h3 button alert expanded large text-center">
+                  <i class="fas fa-palette fa-2x" style="margin-bottom: 10px;"></i><br/>Arts &amp; Humanities</a>
               </div>
             </li>
          </ul>
       </div>
       </div>
       </div>
-        <div class="columns large-7">
+        <div class="columns large-6">
         <div class="hide-for-large">
-         <h2>Select a Career Pathway</h2>
-          <ul class="dropdown menu" id="filters-small" data-dropdown-menu>
-          <li>
-              <a href="#" data-filter="*">
-                <i class="fas fa-asterisk" style="margin-bottom: 10px;"></i>All Programs               
-              </a>
+         <button class="button expanded text-center alert" type="button" data-toggle="example-dropdown-1" data-close-on-click="true">Select a Career Pathway</button>
+<div class="dropdown-pane" id="example-dropdown-1" data-dropdown data-hover="false" data-hover-pane="false" style="min-width: 100%; text-align: center;">
+  <ul id="filters-small" style="list-style-type: none; margin-left: 0;">
+              <li style="list-style: none;">
+                  <a href="#" data-filter="*" class="text-center">
+                    <div class="callout alert">
+                  <i class="fas fa-asterisk" style="margin-right: 10px;"></i> All Programs
+                 </div>
+                  </a>
               </li>
-              <?php
-              foreach ( $program_taxs as $program_tax_slug => $program_tax_name):   //for each term: ?>
-          <li>
-              <a data-filter=".<?php echo $program_tax_slug; ?>"><i class="<?php echo $program_icon->term ?>" style="margin-bottom: 10px;"></i><?php echo $program_tax_name;  ?>
-            </a>
-          </li>
-            <?php endforeach;?>
-      </ul>
+              <li style="list-style-type: none;">
+                  <a data-filter=".business-pathway" cclass="text-center">
+                    <div class="callout alert">
+                  <i class="fas fa-briefcase" style="margin-right: 10px;"></i>Business
+                
+  </div>
+
+                </a>
+             
+            </li>
+              <li style="list-style-type: none;">
+                  <a data-filter=".public-service-pathway" class="text-center">
+                    <div class="callout alert">
+                  <i class="fas fa-gavel" style="margin-right: 10px;"></i> Public Service
+                </div>
+                </a>
+            </li>
+              <li style="list-style-type: none;">
+                  <a data-filter=".it-and-technical-studies-pathway" class="text-center">
+                    <div class="callout alert">
+                  <i class="fas fa-laptop-code" style="margin-right: 10px;"></i> IT &amp; 
+                 Cybersecurity
+                </div>
+               </a>
+            </li>
+              <li style="list-style-type: none;">
+                  <a data-filter=".health-science-pathway" class="text-center">
+                  <div class="callout alert">
+                  <i class="fas fa-user-nurse" style="margin-right: 10px;"></i> Healthcare
+              </div>
+                </a>
+            </li>
+              <li style="list-style-type: none;">
+                  <a data-filter=".social-science-and-education" class="text-center">
+                    <div class="callout alert">
+                  <i class="fas fa-school" style="margin-right: 10px;"></i> Education &amp; Social Science
+                </div>
+                </a>
+            </li>
+               <li style="list-style-type: none;">
+                  <a data-filter=".science-and-engineering-pathway" class="text-center">
+                    <div class="callout alert">
+                  <i class="fas fa-atom" style="margin-right: 10px;"></i> Science &amp; Engineering
+                </div></a>
+            </li>
+            <li style="list-style-type: none;">
+                  <a data-filter=".humanities-and-arts-pathway" class="text-center">
+                    <div class="callout alert">
+                  <i class="fas fa-palette" style="margin-bottom: 10px;"></i> Arts &amp; Humanities</div></a>
+            </li>
+         </ul>
+</div>
      </div>
           <div class="callout" data-equalizer-watch>
-            <?php the_content(); ?>
+            
+        <div class="row expanded collapse" data-equalizer>
+<div class="columns medium-6" style="padding-left: 0;">
+<div class="callout" data-equalizer-watch>
+<div class="row">
+<div class="columns small-1"><i class="fa fa-chalkboard-teacher" style="margin-top:10px;"></i></div>
+<div class="columns small-11"><p class="h4">Online</p>
+<p>Available completely online.</p></div>
+</div>
+<div class="row">
+<div class="columns small-1"><i class="fa fa-running" style="margin-top:10px;"></i></div>
+<div class="columns small-11"><p class="h4">Accelerated</p>
+<p>Complete within 1 year.</p>
+</div>
+</div>
+<div class="row">
+<div class="columns small-1"><i class="fas fa-random" style="margin-top:10px;"></i></div>
+<div class="columns small-11"><p class="h4">Transfer</p>
+<p>Finish at a 4-year college or university.</p></div>
+</div>
+<div class="row">
+<div class="columns small-1"><i class="fa fa-dollar-sign" style="margin-top:10px;"></i></div>
+<div class="columns small-11"><p class="h4">Financial Aid Eligible</p>
+<p>Use your financial aid awards with.</p>
+</div>
+</div>
+</div>
+</div>
+<div class="columns medium-6">
+<div class="callout" data-equalizer-watch>
+<ul class="accordion" data-accordion data-allow-all-closed="true">
+  <li class="accordion-item" data-accordion-item>
+    <a href="#" class="accordion-title">Associate of Arts & Sciences Degree (AA&S)</a>
+    <div class="accordion-content" data-tab-content>
+  <p>This degree is appropriate for those students who plan to transfer to four-year institutions.</p>
+</div>
+</li>
+<li class="accordion-item" data-accordion-item>
+    <a href="#" class="accordion-title">Associate of Science (AS):</a>
+    <div class="accordion-content" data-tab-content>
+<p>This degree is appropriate for those students who plan to transfer to four-year institutions.</p>
+</div>
+</li>
+<li class="accordion-item" data-accordion-item>
+    <a href="#" class="accordion-title">Associate of Applied Science Degree (AAS):</a>
+    <div class="accordion-content" data-tab-content>
+<p>Students who plan to obtain full-time employment upon graduation from Germanna.</p>
+</div>
+</li>
+<li class="accordion-item" data-accordion-item>
+    <a href="#" class="accordion-title">Certificate (CERT):</a>
+    <div class="accordion-content" data-tab-content>
+<p>Awarded to students who complete one of the approved non-degree curricula which are is usually less than two years in length.</p>
+</div>
+</li>
+<li class="accordion-item" data-accordion-item>
+  <a href="#" class="accordion-title">Career Studies Certificate (CSC)</a>
+  <div class="accordion-content" data-tab-content>
+  <p>Career studies programs are developed and implemented in response to community needs when institutional resources permit.</p>
+  </div>
+</li>
+
+</ul>
+</div>
+</div>
+</div>
+ 
          </div>
         </div>
       </div>
