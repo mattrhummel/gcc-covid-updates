92bc1f3f88012f96f3d9b5157717d69cae692fce
Matt Hummel <marketing@users.noreply.github.com>
2020-08-17 13:03:19 -0400

    update
    revise covide template

--- page-covid.php
+++ page-covid.php
@@ -10,36 +10,102 @@
 *
 */
 get_header(); ?>
+get_header(); ?>
+<?php
+while ( have_posts() ) : the_post(); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   
-  <?php
-  while ( have_posts() ) : the_post(); ?>
-
-   <div class="row expanded content-area">
-
-    <div class="mobile-sidebar" data-responsive-toggle="section-menu" data-hide-for="large">
-      
-      <button class="button expanded mobile-sidebar-button" type="button" data-toggle="section-menu"><?php _e('In this Section', 'gcc-wp-2018'); ?>
-      </button>
-      
-    </div>
-
-      <aside class="columns small-12 large-3 float-right page-nav hide-for-print" id="section-menu">
-        <?php dynamic_sidebar( 'covid-widgets' ); ?>
-      </aside>
-
-      <div class="columns small-12 large-9 float-left">
-      
-       <?php //Page Heading
-        get_template_part( 'template-parts/content', 'page-heading' );
-       ?>
-
+  <div class="content-area">
+    
+    <div class="entry-content" id="main">
+      <div class="row expanded">
+        <div class="columns small-12">
+          
+          <header>
+              <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+              <?php the_breadcrumb() ?> 
+          </header>
 
         </div>
-      
+      </div>
+      <div class="row expanded" style="padding-left: 10px; padding-right: 10px;">
+
+        <?php //Page Heading
+          get_template_part( 'template-parts/content', 'page-alert' );
+        ?>
+
       </div>
 
-      <?php endwhile; // End of the loop. ?>
-    </article>
-    <?php
-    get_footer();
+      <?php
+      the_content();
+      ?>
+
+ <div class="row expanded">
+<div class="column medium-4">
+<div class="callout large">
+
+  <style>
+    .widget:first-of-type {
+    background-color: #f9f9f9;
+
+}
+.widget h3 {
+    padding: 1rem 1rem 1rem 0;
+}
+
+  </style>
+
+   <?php dynamic_sidebar( 'covid-widgets' ); ?>
+
+</div>
+</div>
+<div class="column medium-8">
+<div class="callout large">
+
+      <?php
+          $args =  array (
+          'post_type' => 'covid_19',
+          'post_status' => 'publish',
+          'orderby' => 'publish_date',
+          'order' => 'DESC',
+          'posts_per_page'=> -1,
+        );
+          ?>
+
+
+<?php
+          $query = new WP_Query( $args ); ?>
+          <?php if ( $query->have_posts() ) : ?>
+
+<h2 class="h3">Latest College Updates</h2>
+
+  <ul class="no-bullet">
+
+      <?php while ( $query->have_posts() ) : $query->the_post();?>
+      
+      <li>
+        <h3 class="h4">
+          <a class="<?php echo get_the_permalink(); ?>"><span class="subheader"><?php echo get_the_date(); ?></span><br />
+           <?php the_title(); ?>
+         </a>
+       </h3>
+      </li>
+
+    <?php endwhile; ?>
+
+          <?php wp_reset_postdata(); ?>
+          <?php else : ?>
+          <?php endif; ?>
+
+  </ul>
+</div>
+</div>
+</div>
+           
+
+    </div>
+  </div>
+</article>
+<?php endwhile; // End of the loop. ?>
+<?php
+get_footer();
