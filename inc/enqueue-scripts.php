a948d7547d3504c5c2eeec16ecbe260a18765818
Matt Hummel <marketing@users.noreply.github.com>
2020-08-25 08:17:41 -0400

    updatee
    remove lazy load script

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -1,7 +1,7 @@
 <?php
 function gcc_wp_2018_scripts() {
 
-wp_enqueue_style( 'gcc-wp-2018-theme-styles' , get_template_directory_uri() . '/dist/css/style.min.css', array(), '20200819', 'all' );
+wp_enqueue_style( 'gcc-wp-2018-theme-styles' , get_template_directory_uri() . '/dist/css/style.min.css', array(), '20200825', 'all' );
 
 wp_enqueue_style('gcc-wp-2018-motion-ui', '//cdnjs.cloudflare.com/ajax/libs/motion-ui/1.1.1/motion-ui.min.css', array(), '2020605', 'all');
 
@@ -12,8 +12,6 @@ wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquer
 wp_deregister_script( 'jquery-migrate' );
 
 wp_register_script( 'jquery-migrate', '//code.jquery.com/jquery-migrate-3.0.1.min.js', array('jquery'), '3.0.1', true );
-
-wp_enqueue_script( 'gcc-wp-2018-lazy-load', get_template_directory_uri() . '/dist/scripts/jquery.lazy.min.js', array(), '', true );
 
 wp_enqueue_script( 'gcc-wp-2018-foundation', '//cdnjs.cloudflare.com/ajax/libs/foundation/6.6.1/js/foundation.min.js', array(), '20191025', true );
 
