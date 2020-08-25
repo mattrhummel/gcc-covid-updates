2e92a14d84faf127ce2ed0d37c3d546f7a7efe22
Matt Hummel <marketing@users.noreply.github.com>
2020-08-25 16:06:07 -0400

    update
    fix label margin

--- page-pathways-template.php
+++ page-pathways-template.php
@@ -147,7 +147,7 @@ while ( have_posts() ) : the_post(); ?>
 <div class="columns medium-5">
   <style>
     .label {
-      margin-bottom:  1rem;
+      margin-bottom:  .5rem;
     }
   </style>
 <div class="callout alert large" data-equalizer-watch>
