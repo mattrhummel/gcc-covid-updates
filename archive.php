2df90a70c30590bf1ba7c0f0972d705882edc9d3
Matt Hummel <marketing@users.noreply.github.com>
2020-09-02 11:15:29 -0400

    update
    archive

--- archive.php
+++ archive.php
@@ -23,7 +23,7 @@ $post_page_title= get_field('post_page_title', 'option');
     <div class="columns small-12 large-9 float-left">
        
      <header>
-       <h1><?php echo the_archive_title(); ?></h1>
+        <h1><?php echo the_archive_title(); ?></h1>
                 <nav aria-label="<?php _e('You are here:', 'gcc-wp-2018');?>" role="navigation">
           <div title="breadcrumbs trail">
             <ul class="breadcrumbs">
@@ -40,10 +40,10 @@ $post_page_title= get_field('post_page_title', 'option');
         </nav>
     </header>
 
-      <?php $custom_terms = get_terms('cw_event_categories'); ?>
+    <?php $custom_terms = get_terms('cw_event_categories'); ?>
 
 
-  <?php foreach($custom_terms as $custom_term) {
+    <?php foreach($custom_terms as $custom_term) {
     wp_reset_query();
     $args = array('post_type' => 'cw_events',
         'tax_query' => array(
@@ -55,13 +55,15 @@ $post_page_title= get_field('post_page_title', 'option');
         ),
      ); ?>
 
+     <?php do_action('show_beautiful_filters'); ?>
+
     <?php $loop = new WP_Query($args);
      
     if($loop->have_posts()) { 
 
      while($loop->have_posts()) : $loop->the_post();
 
-      ?>
+    ?>
 
             <div class="callout primary">
               
