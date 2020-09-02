d4f91749af1e44570bce0426258487c42a8361a6
Matt Hummel <marketing@users.noreply.github.com>
2020-09-02 10:52:12 -0400

    update
    pathways template

--- page-pathways-template.php
+++ page-pathways-template.php
@@ -66,8 +66,8 @@ $query = new WP_Query( $args );
 
   if($query->have_posts()) { 
 
-
-     while($query->have_posts()) : $query->the_post();
+    while($query->have_posts()) : $query->the_post();
+      
 ?>
 
 <div class="row expanded" data-equalizer>
