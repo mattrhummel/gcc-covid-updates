c99347cffaa5894e3d0d7dc769908b4312e138f6
Matt Hummel <marketing@users.noreply.github.com>
2020-09-15 07:50:30 -0400

    update
    fixt foundation

--- footer.php
+++ footer.php
@@ -96,7 +96,8 @@ function getRandomImage(imgAr, path) {
 <script>
       $(document).foundation();
   window.almComplete = function(alm){
-  new Foundation.reInit($(".tooltip, data-equalizer"));
+  new Foundation.reInit($(".tooltip, .accordion, data-equalizer"));
+  
   new Foundation.Equalizer($("#ajax-load-more")).getHeightsByRow(resetHeights);
   };
   function resetHeights(groups) {
